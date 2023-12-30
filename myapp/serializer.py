3f890513659fed90651db0d09915bf59e32c547a
safaamostafa <safamos21@gmail.com>
2023-12-30 19:21:38 +0200

    serializer coding

---
+++ myapp/serializer.py
@@ -0,0 +1,24 @@
+from dataclasses import fields
+from itertools import count
+from pyexpat import model
+from rest_framework.serializers import ModelSerializer,SerializerMethodField
+from.models import Advocate,Company
+
+
+class CompanySerializer(ModelSerializer):
+    employee_count = SerializerMethodField(read_only=True)
+    class Meta:
+       model = Company
+       fields = '__all__'  
+
+    def get_employee_count(self,obj):
+           count = obj.advocate_set.count()
+           return count
+
+class AdvocateSerializer(ModelSerializer) :
+    company = CompanySerializer()
+    class Meta:
+        model = Advocate
+        fields = ['username','bio','company']
+
+   
